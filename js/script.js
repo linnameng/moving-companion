6eb45c6bc76d5ef45000416e7259a6bd2a2232d0
Linna Meng <linna.meng@bluespurs.com>
2015-02-02 14:30:58 -0400

    Initial commit

--- js/script.js
+++ js/script.js
@@ -11,60 +11,29 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
-    var streetStr = $('#street').val();
-    var cityStr = $('#city').val();
-    var address = streetStr + ', ' + cityStr;
-
-    $greeting.text('So, you want to live at ' + address + '?');
-
     // load streetview
-    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
-    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+    var street = $('#street').val();
+    var city = $('#city').val();
+    var address = street + ', ' + city;
+
+    var streetViewUrl = "https://maps.googleapis.com/maps/api/streetview?size=640x640&location="+address;
+
+    $body.append('<img class="bgimg" src="'+streetViewUrl+'">');
+
+    //var nytimesUrl = "http://api.nytimes.com/svc/search/v2/articlesearch.json?fq="+city+"&api-key=fbd65ef35763ea4c3f29303cee143e3c:13:71150372";
+    var nytUrl = "http://api.nytimes.com/svc/search/v2/articlesearch.json?";
+    var nytApiKey = "fbd65ef35763ea4c3f29303cee143e3c:13:71150372";
+
+    $.getJSON( nytUrl, {
+        fq: city,
+        api-key: nytApiKey
+    }
+    var articles = data.response.docs;
+    
+
+    );
 
 
-    // load nytimes
-    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=3abc9a3d23e60b38c21b4ab9b0a91c07:17:69911633'
-    $.getJSON(nytimesUrl, function(data){
-
-        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
-
-        articles = data.response.docs;
-        for (var i = 0; i < articles.length; i++) {
-            var article = articles[i];
-            $nytElem.append('<li class="article">'+
-                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
-                '<p>' + article.snippet + '</p>'+
-            '</li>');
-        };
-
-    }).error(function(e){
-        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
-    });
-
-
-
-    // load wikipedia data
-    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
-    var wikiRequestTimeout = setTimeout(function(){
-        $wikiElem.text("failed to get wikipedia resources");
-    }, 8000);
-
-    $.ajax({
-        url: wikiUrl,
-        dataType: "jsonp",
-        jsonp: "callback",
-        success: function( response ) {
-            var articleList = response[1];
-
-            for (var i = 0; i < articleList.length; i++) {
-                articleStr = articleList[i];
-                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
-                $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
-            };
-
-            clearTimeout(wikiRequestTimeout);
-        }
-    });
 
     return false;
 };
